db7428434be1504f3a02436823f98c7afffcb3f4
alias <matusoyo2436@gmail.com>
2025-05-15 22:32:08 +0900

    add             $table->string('name', 50)->unique(); // 職務名

--- TechBoard/database/migrations/2025_05_15_201303_create_job_types_table.php
+++ TechBoard/database/migrations/2025_05_15_201303_create_job_types_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('job_types', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50)->unique(); // 職務名
             $table->timestamps();
         });
     }

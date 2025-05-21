6baece48cc2e204e1daddeb4bedee512a598bed3
alias <matusoyo2436@gmail.com>
2025-05-21 17:56:16 +0900

    Technology.php 実装

--- TechBoard/app/Models/Technology.php
+++ TechBoard/app/Models/Technology.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Technology extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function jobListings(): BelongsToMany
+    {
+        return $this->belongsToMany(JobListing::class, 'job_technology')->withTimestamps();
+    }
+
 }

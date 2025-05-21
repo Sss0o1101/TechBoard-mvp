0e823f8abc1d32a3075a54f454ae8dcb78d46225
alias <matusoyo2436@gmail.com>
2025-05-21 17:57:04 +0900

    Favorite.php 実装

--- TechBoard/app/Models/Favorite.php
+++ TechBoard/app/Models/Favorite.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Favorite extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'job_listing_id',
+        'comment',
+        'commented_at',
+    ];
+
+    protected $casts = [
+        'commented_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function jobListing(): BelongsTo
+    {
+        return $this->belongsTo(JobListing::class);
+    }
+
+    public function getHasCommentAttribute(): bool
+    {
+        return !is_null($this->comment);
+    }
 }

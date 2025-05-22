9adf17d2f2d1e33b90bd7201efc599be264b6fe3
alias <matusoyo2436@gmail.com>
2025-05-22 18:40:08 +0900

    JobTypeSeeder.php

--- TechBoard/database/seeders/JobTypeSeeder.php
+++ TechBoard/database/seeders/JobTypeSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\JobType;
+use App\Traits\LoadsSeederData;
 use Illuminate\Database\Seeder;
 
 class JobTypeSeeder extends Seeder
 {
+    use LoadsSeederData;
+
     /**
-     * Run the database seeds.
+     * 職種データのシード
      */
     public function run(): void
     {
-        //
+        // マスターデータを読み込む
+        $jobTypes = $this->loadData('master/job_types.json');
+
+        // データ登録
+        foreach ($jobTypes as $jobTypeData) {
+            JobType::create($jobTypeData);
+        }
     }
 }

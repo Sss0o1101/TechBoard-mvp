ab5de105cbb12b90bf307f42aa08a9e7807a348d
alias <matusoyo2436@gmail.com>
2025-05-22 18:40:26 +0900

    PrefectureSeeder.php

--- TechBoard/database/seeders/PrefectureSeeder.php
+++ TechBoard/database/seeders/PrefectureSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Prefecture;
+use App\Traits\LoadsSeederData;
 use Illuminate\Database\Seeder;
 
 class PrefectureSeeder extends Seeder
 {
+    use LoadsSeederData;
+
     /**
-     * Run the database seeds.
+     * 都道府県データのシード
      */
     public function run(): void
     {
-        //
+        // マスターデータを読み込む
+        $prefectures = $this->loadData('master/prefectures.json');
+
+        // データ登録
+        foreach ($prefectures as $prefectureData) {
+            Prefecture::create($prefectureData);
+        }
     }
 }

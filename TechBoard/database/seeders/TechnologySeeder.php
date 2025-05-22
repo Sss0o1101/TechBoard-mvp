685d1a519013fea56817310f8045d2f74fe43b0f
alias <matusoyo2436@gmail.com>
2025-05-22 18:39:56 +0900

    TechnologySeeder.php

--- TechBoard/database/seeders/TechnologySeeder.php
+++ TechBoard/database/seeders/TechnologySeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Technology;
+use App\Traits\LoadsSeederData;
 use Illuminate\Database\Seeder;
 
 class TechnologySeeder extends Seeder
 {
+    use LoadsSeederData;
+
     /**
-     * Run the database seeds.
+     * 技術スタックデータのシード
      */
     public function run(): void
     {
-        //
+        // マスターデータを読み込む
+        $technologies = $this->loadData('master/technologies.json');
+
+        // データ登録
+        foreach ($technologies as $technologyData) {
+            Technology::create($technologyData);
+        }
     }
 }

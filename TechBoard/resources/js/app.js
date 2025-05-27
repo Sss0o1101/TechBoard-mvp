9320eeeded5199b6af984aaf98905bcf658a8b89
alias <matusoyo2436@gmail.com>
2025-05-27 23:05:53 +0900

    added new text

--- TechBoard/resources/js/app.js
+++ TechBoard/resources/js/app.js
@@ -1,7 +1,18 @@
 import './bootstrap';
+import { initFavoriteButtons, showToast } from './favoriteHandler';
+import { initFavoriteComments } from './favoriteCommentHandler';
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
+// グローバルで利用できるようにする
+window.showToast = showToast;
+
 Alpine.start();
+
+// DOMコンテンツ読み込み後に初期化
+document.addEventListener('DOMContentLoaded', function() {
+    initFavoriteButtons();
+    initFavoriteComments();
+});

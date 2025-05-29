88573d9be2e6535a049e69ebacb1accf141a1ff9
alias <matusoyo2436@gmail.com>
2025-05-29 23:46:50 +0900

    トースト通知のための再利用可能なコンポーネントを作成。

---
+++ TechBoard/resources/views/components/toast-notification.blade.php
@@ -0,0 +1,3 @@
+<div id="toast" class="fixed bottom-5 right-5 bg-green-500 text-white px-4 py-2 rounded shadow-lg transform transition-transform duration-300 translate-y-full opacity-0 z-50">
+    <span id="toastMessage"></span>
+</div>
